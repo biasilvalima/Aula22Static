054e32d1064befd2d7adab412604e234c71a4838
Beatriz <biasilva.lima118@gmail.com>
2020-06-22 10:15:38 -0300

    Clases statics aplicadas

--- Conversor.cs
+++ Conversor.cs
@@ -1,7 +1,28 @@
 namespace Aula22Static
 {
-    public class Conversor
+    public static class Conversor
     {
-        
+        public static float CotacaoDolar = 5.25f;
+        public static float CotacaoEuro = 5.89f;
+
+        public static float ConverterDolarParaReal(float valor)
+        {
+            return valor * CotacaoDolar;
+        }
+
+        public static float ConverterRealParaDolar(float valor)
+        {
+            return valor / CotacaoDolar;
+        }
+
+        public static float CoverterEuroParaReal(float valor)
+        {
+            return valor * CotacaoEuro;
+        }
+
+        public static float ConverterRealParaEuro(float valor)
+        {
+            return valor / CotacaoEuro;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Aula22Static
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.DarkBlue
+            ;
+
+            Conversor.CotacaoDolar = 5.25f;
+            System.Console.WriteLine("O valor de U$15 em r$ é de: ");
+            System.Console.WriteLine( Conversor.ConverterDolarParaReal (15));
+
+            System.Console.WriteLine("O valor de r$50 em U$ é de: ");
+            System.Console.WriteLine( Conversor.ConverterRealParaDolar (50));
+
+            System.Console.WriteLine();
+
+            Conversor.CotacaoEuro = 5.89f;
+            System.Console.WriteLine("O valor de £15 em r$ é de: ");
+            System.Console.WriteLine( Conversor.CoverterEuroParaReal (15));
+
+            System.Console.WriteLine("O valor de r$50 em £ é de: ");
+            System.Console.WriteLine( Conversor.ConverterRealParaEuro(50));
+
+            Console.ResetColor();
         }
     }
 }
